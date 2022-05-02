7d5acb5e68e35f2c72685ba4c3da3781a641831a
aks-cefz7k <www_qdhc@yeah.net>
2022-05-03 03:37:48 +0800

    增加系统配置API

---
+++ OpenATC-Admin-server/agent/src/main/java/com/openatc/agent/model/SystemConfig.java
@@ -0,0 +1,20 @@
+package com.openatc.agent.model;//package com.kedacom.kiss.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "systemconfig", schema = "public")
+public class SystemConfig {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String module;
+
+    private String key;
+
+    private String value;
+}
---
+++ OpenATC-Admin-server/agent/src/main/java/com/openatc/agent/service/SystemConfigDao.java
@@ -0,0 +1,10 @@
+package com.openatc.agent.service;
+
+import com.openatc.agent.model.SystemConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SystemConfigDao extends JpaRepository<SystemConfig, Long> {
+    List<SystemConfig> findAllByModule(String Module);
+}
