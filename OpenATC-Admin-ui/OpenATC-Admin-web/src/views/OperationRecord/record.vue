306f2032f366903679f651e3ee77e2e28bf2b922
aks-cefz7k <www_qdhc@yeah.net>
2023-05-04 12:57:32 +0800

    添加操作记录类型翻译说明。

--- OpenATC-Admin-ui/OpenATC-Admin-web/src/views/OperationRecord/record.vue
+++ OpenATC-Admin-ui/OpenATC-Admin-web/src/views/OperationRecord/record.vue
@@ -96,6 +96,8 @@ export default {
       tableData: [],
       statusMode: new Map([['set-request', '请求错误'], ['set-response', '应答成功'], ['error-response', '应答错误']]),
       statusModeEn: new Map([['set-request', 'Request error'], ['set-response', 'Successful response'], ['error-response', 'Response error']]),
+      infotypeMode: new Map([['control/pattern', '控制消息'], ['control/interrupt', '方案控制消息'], ['control/preempt', '优先控制'], ['control/channelcheck', '通道检测'], ['control/remote', '远程控制'], ['system/centerip', '中心ip地址'], ['system/ip1', '设备ip地址1'], ['system/ip2', '设备ip地址2'], ['system/serialport', '串口信息'], ['system/time', '系统时间'], ['system/remote', '远程调试'], ['system/update', '系统更新'], ['system/paramversion', '参数版本'], ['system/optstatparam', '优化统计参数'], ['system/faultclear', '故障消除'], ['system/udiskupdate', '更新U盘数据'], ['system/volumelog', '交通流量历史数据'], ['feature/channel', '通道'], ['feature/phase', '相位'], ['feature/overlap', '跟随相位'], ['feature/pattern', '方案'], ['feature/plan', '计划'], ['feature/date', '日期'], ['feature/peddetecter', '行人检测器'], ['feature/devinfo', '设备信息'], ['feature/manualpanel', '手动面板配置'], ['feature/channellock', '通道灯色锁定参数配置'], ['feature/all', '整体参数']]),
+      // infotypeModeEn: new Map([['control/pattern', '控制消息'], ['control/interrupt', '方案控制消息'], ['control/preempt', '优先控制'], ['control/channelcheck', '通道检测'], ['control/remote', '远程控制'], ['system/centerip', '中心ip地址'], ['system/ip1', '设备ip地址1'], ['system/ip2', '设备ip地址2'], ['system/serialport', '串口信息'], ['system/time', '系统时间'], ['system/remote', '远程调试'], ['system/update', '系统更新'], ['system/paramversion', '参数版本'], ['system/optstatparam', '优化统计参数'], ['system/faultclear', '故障消除'], ['system/udiskupdate', '更新U盘数据'], ['system/volumelog', '交通流量历史数据'], ['feature/channel', '通道'], ['feature/phase', '相位'], ['feature/overlap', '跟随相位'], ['feature/pattern', '方案'], ['feature/plan', '计划'], ['feature/date', '日期'], ['feature/peddetecter', '行人检测器'], ['feature/devinfo', '设备信息'], ['feature/manualpanel', '手动面板配置'], ['feature/channellock', '通道灯色锁定参数配置'], ['feature/all', '整体参数']]),
       listQuery: {
         pageNum: 1, // 页码
         pageRow: 50 // 每页条数
@@ -152,6 +154,7 @@ export default {
     getAllRecord () {
       this.listLoading = true
       getHisParamsList(this.listQuery.pageNum, this.listQuery.pageRow).then(data => {
+        debugger
         if (data.data.success !== true) {
           this.listLoading = false
           if (data.data.code === '20004') {
@@ -190,7 +193,11 @@ export default {
             } else {
               obj[key] = that.statusMode.get(obj[key])
             }
-            // obj[key] = that.statusMode.get(obj[key])
+          }
+          if (key === 'infotype') {
+            if (that.$i18n.locale === 'zh') {
+              obj[key] = that.infotypeMode.get(obj[key])
+            }
           }
         })
       }
