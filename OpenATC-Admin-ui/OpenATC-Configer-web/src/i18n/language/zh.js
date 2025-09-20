ae66d1518407aa23b279d832d7041c4d20275845
aks-cefz7k <www_qdhc@yeah.net>
2025-09-20 11:26:11 +0800

    修改故障、日志、统计接口及调用； 获取历史数据方式均改为点击按钮后上载； 增加等待loading，并设置30s自动关闭； 故障、日志、统计界面高度自适应调整； 统计界面检测器按钮组优化为下拉框显示； 解决统计界面折线图大小自适应不生效问题； 删除用户名密码；

--- OpenATC-Admin-ui/OpenATC-Configer-web/src/i18n/language/zh.js
+++ OpenATC-Admin-ui/OpenATC-Configer-web/src/i18n/language/zh.js
@@ -636,6 +636,7 @@ const zh = {
     },
     'fault': {
       'refresh': '刷新',
+      'uploadfault': '上载故障',
       'export': '导出',
       'faultofcurrentdetailedlightgroup': '当前详细灯组故障',
       'faultofcurrentdetailedvehicleinspectionversion': '当前详细车检版故障',
@@ -657,7 +658,7 @@ const zh = {
       'pass': '密码：',
       'usernameplaceholder': '请输入用户名',
       'passplaceholder': '请输入密码',
-      'getdevicedata': '获取设备流量数据',
+      'getdevicedata': '上载流量',
       'detector': '检测器',
       'time': '时间',
       'flow': '流量',
@@ -671,7 +672,8 @@ const zh = {
       'infotype': '事件类型',
       'subject': '主体标时',
       'object': '客体标时',
-      'operationresults': '操作结果'
+      'operationresults': '操作结果',
+      'uploaddailyrecord': '上载日志'
     },
     'control': {
       'mode': '模式:',
