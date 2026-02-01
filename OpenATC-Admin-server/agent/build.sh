eb5b7d28b26042f5fd2e8f0ec84f8d0d69b82ea5
aks-cefz7k <www_qdhc@yeah.net>
2026-02-01 12:18:23 +0800

    1 更新comm日志记录

--- OpenATC-Admin-server/agent/build.sh
+++ OpenATC-Admin-server/agent/build.sh
@@ -1,2 +1,4 @@
 docker build -t docker-registry:5000/openatc/admin:latest .
 docker push docker-registry:5000/openatc/admin:latest
+docker build -t docker-registry:5000/openatc/admin:1.20.1 .
+docker push docker-registry:5000/openatc/admin:1.20.1
